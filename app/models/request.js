d5658bd35bb61d25808ccd1a79d56ed939f6bbb2
github username <github email address>
2016-11-14 12:09:51 -0500

    heroku commits

--- app/models/request.js
+++ app/models/request.js
@@ -1,37 +1,51 @@
 // var request = require('request');
 // var OAuth   = require('oauth-1.0a');
 // var crypto  = require('crypto');
-const rootURL = "https://api.twitter.com/1.1/search/tweets.json?q="
+const rootURL = "http://api.eventful.com/rest/events/search?app_key=btZGcN2g4drGjd73&keywords="
 
 function queryTwitterAPI(){
   let term = $("#userInput").val()
   debugger
   $.ajax({
     url: rootURL + term,
-    data: oauth.authorize(request_data, token),
-    headers: oauth.toHeader(oauth.authorize(request_data, token))
-    }).done(function(data) {
-    console.log(data)
-  });
-}
-var oauth = OAuth({
-    consumer: {
-        key: "2Q858yg1xnvUIWjPPTqDB3Ldc",
-        secret: 'AW51lNLM62xR7Gotl8UwDpApqCkR3D5lxyqROGGyK9RiWyM2Oi'
-    },
-    signature_method: 'HMAC-SHA1',
-    hash_function: function(base_string, key) {
-        return CryptoJS.HmacSHA1(base_string, key).toString(CryptoJS.enc.Base64);
+    // dataType: 'jsonp',
+    // jsonpCallback: false,
+    // data: oauth.authorize(request_data, token),
+    headers: {
+      "Access-Control-Allow-Origin": '*'
     }
-});
-var token = {
-    key: '797438164193472516-aQ5yiVeki8LJBbxXEwiZAcz3gICEsWO',
-    secret: 'abkosoNRF2UaOFQHtpUSGnW91c2w51yhvSCIX64tNc0vH'
-};
-var request_data = {
-    url: "https://api.twitter.com/1.1/search/tweets.json?q=Election",
-    method: "GET"
-};
+  }).done(function(data){
+    console.log(data)
+  })
+    // error: function(xhr, status, error){
+    //   console.log(xhr)
+    //   console.log(status),
+    //   console.log(error)
+    // }
+
+}
+
+// var oauth = OAuth({
+//     consumer: {
+//         key: "2Q858yg1xnvUIWjPPTqDB3Ldc",
+//         secret: 'AW51lNLM62xR7Gotl8UwDpApqCkR3D5lxyqROGGyK9RiWyM2Oi'
+//     },
+//     signature_method: 'HMAC-SHA1',
+//     hash_function: function(base_string, key) {
+//         return CryptoJS.HmacSHA1(base_string, key).toString(CryptoJS.enc.Base64);
+//     }
+// });
+// var token = {
+//     key: '797438164193472516-aQ5yiVeki8LJBbxXEwiZAcz3gICEsWO',
+//     secret: 'abkosoNRF2UaOFQHtpUSGnW91c2w51yhvSCIX64tNc0vH'
+// };
+// var request_data = {
+//     url: "https://api.twitter.com/1.1/search/tweets.json?q=",
+//     method: "GET"
+// };
+// function callBack(json){
+//   console.log(json)
+// }
 // $.ajax({
 //     url: request_data.url,
 //     type: request_data.method,
@@ -57,7 +71,12 @@ var request_data = {
 //     url: request_data.url,
 //     method: request_data.method,
 //     form: request_data.data,
+//     oauth.toHeader(oauth.authorize(request_data, token)),
 //     headers: oauth.toHeader(oauth.authorize(request_data, token))
 // }, function(error, response, body) {
 //     console.log(response)
 // });
+//        ,
+// xhrFields: {
+//   withCredentials: true
+// }
