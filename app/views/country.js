e333000d3288549ac2bf35cdced763e3f72ee987
slindquist3 <slindquist3@gmail.com>
2016-11-15 08:34:22 -0500

    with country model

--- app/views/country.js
+++ app/views/country.js
@@ -1,27 +1,8 @@
-// 1.
-	// make our country classt
-// 2. store
-	// Each instance created, it should populate the store.
-//  3. Use the adapter pattern
-	// to take data from our .json file, and instead populate my store with the instances of a country.
+function getCountry(){
+  var country = document.getElementById('country').value;
+}
 
-
-let store = [];
-const countryFactory = (function() {
-  let id = 0
-  return class Country {
-    constructor(name, latlng) {
-      this.id = ++id;
-      this.name = name;
-      this.latlng = latlng;
-      store = [...store, this]
-    }
-  }
-})()
-
-countries.map(function(country) {
-  return new countryFactory(country.name.common, country.latlng)
-})
-
-
-// select store.latlng where store.name == "" value or title
+//1. collect selected country
+//2. move return value of getCountry function into the area where we determine map coordinates
+//3. access return value of getCountry function coordinates
+//4. Account for if no country is selected. 
